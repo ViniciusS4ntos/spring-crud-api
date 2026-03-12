1310fff2fdefed9f824b22deb6e90da36615c153
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-03-11 22:40:11 -0300

    ajustes finais

--- src/main/java/com/vinicius/spring_crud_api/infrastructure/entity/User.java
+++ src/main/java/com/vinicius/spring_crud_api/infrastructure/entity/User.java
@@ -13,7 +13,7 @@ import lombok.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     @Column(name = "email", unique = true)
     private String email;
